f060066ea3d964f3ff8adc939330390065fcfbce
2000kevin <kevin.perdomo.oliva@utelvt.edu.ec>
2022-09-24 01:52:56 +0000

    programa de calculadora.cpp modificado con variable distinta

--- calculadora.cpp
+++ calculadora.cpp
@@ -1,19 +1,21 @@
+//modificado por kevin perdomo oliva
 #include<iostream>
 using namespace std;
 
 int main()
 {
-	float x,y,s,r,d,m;
-	cout<<"ingrese el valor de x="; cin>>x;
-	cout<<"ingrese el valor de y="; cin>>y;
-	s=x+y;
-	r=x-y;
-	m=x*y;
-	d=x/y;
-	cout<<"el resultafo de la suma es"<<x<<"+"<<y<<"="<<s<<endl;
-	cout<<"el resultafo de la resta es"<<x<<"-"<<y<<"="<<r<<endl;
-	cout<<"el resultafo de la multiplicacion es"<<x<<"*"<<y<<"="<<m<<endl;
-	cout<<"el resultafo de la dividion es"<<x<<"/"<<y<<"="<<d<<endl;
+	float k,p,s,r,d,m;
+	cout<<"ingrese el valor de k="; cin>>k;
+	cout<<"ingrese el valor de p="; cin>>p;
+	s=k+p;
+	r=k-p;
+	m=k*p;
+	d=k/p;
+	cout<<"el resultafo de la suma es"<<k<<"+"<<p<<"="<<s<<endl;
+	cout<<"el resultafo de la resta es"<<k<<"-"<<p<<"="<<r<<endl;
+	cout<<"el resultafo de la multiplicacion es"<<k<<"*"<<p<<"="<<m<<endl;
+	cout<<"el resultafo de la dividion es"<<k<<"/"<<p<<"="<<d<<endl;
 	return 0;
 }
+// gracias ;)
 
